60230b03a02c52f11008e3c18f9867bd365f8a3a
Ben Grinthal <bgrinthal@gmail.com>
2022-11-18 10:28:04 -0500

    added stickman figure for incorrect guesses and method to search word for guessed characters

--- hangman-game/Program.cs
+++ hangman-game/Program.cs
@@ -1,16 +1,109 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using static System.Random;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace HangmanGame
 {
     class Program
     {
+        private static void printHangman(int wrong)
+        {
+            if (wrong == 0)
+            {
+                Console.WriteLine("\n+---+");
+                Console.WriteLine("    |");
+                Console.WriteLine("    |");
+                Console.WriteLine("    |");
+                Console.WriteLine("   ===");
+                Console.Write("you have 6 guesses");
+            }
+            else if (wrong == 1)
+            {
+                Console.WriteLine("\n+---+");
+                Console.WriteLine("O   |");
+                Console.WriteLine("    |");
+                Console.WriteLine("    |");
+                Console.WriteLine("   ===");
+                Console.Write("You have 5 guesses left");
+            }
+            else if (wrong == 2)
+            {
+                Console.WriteLine("\n+---+");
+                Console.WriteLine("O   |");
+                Console.WriteLine("|   |");
+                Console.WriteLine("    |");
+                Console.WriteLine("   ===");
+                Console.Write("You have 4 guesses left");
+            }
+            else if (wrong == 3)
+            {
+                Console.WriteLine("\n+---+");
+                Console.WriteLine(" O  |");
+                Console.WriteLine("/|  |");
+                Console.WriteLine("    |");
+                Console.WriteLine("   ===");
+                Console.Write("You have 3 guesses left");
+            }
+            else if (wrong == 4)
+            {
+                Console.WriteLine("\n+---+");
+                Console.WriteLine(" O  |");
+                Console.WriteLine("/|\\ |");
+                Console.WriteLine("    |");
+                Console.WriteLine("   ===");
+                Console.Write("You have 2 guesses left");
+            }
+            else if (wrong == 5)
+            {
+                Console.WriteLine("\n+---+");
+                Console.WriteLine(" O  |");
+                Console.WriteLine("/|\\ |");
+                Console.WriteLine("/   |");
+                Console.WriteLine("   ===");
+                Console.Write("You have 1 guesse left");
+            }
+            else if (wrong == 6)
+            {
+                Console.WriteLine("\n+---+");
+                Console.WriteLine(" O   |");
+                Console.WriteLine("/|\\  |");
+                Console.WriteLine("/ \\  |");
+                Console.WriteLine("    ===");
+                Console.Write("Game Over");
+            }
+        }
+
+        // List<char>guessedLetters represents list of objects called "guessedLetters", with the type "char" (A, B, C, D, etc.)
+        private static int printWord(List<char>guessedLetters, String randomWord)
+        {
+            // counter to iterate through random word
+            int counter = 0;
+            // number of correctly guessed letteres
+            int rightLetters = 0;
+            // spacing
+            Console.Write("\r\n");
+            // iterates through each character "c" inside randomWord string
+            foreach (Char c in randomWord)
+            {
+                //  checks if the character in randomWord is inside guessedLetters
+                if(guessedLetters.Contains(c))
+                {
+                    Console.Write(c + " ");
+                    rightLetters++;
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+                counter++;
+            }
+            return rightLetters;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            
         }
     }
 }
