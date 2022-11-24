4d298434e77a0cca0fe7b8c3b34f60f057451647
Ben Grinthal <bgrinthal@gmail.com>
2022-11-24 11:48:21 -0500

    added popular movie titles, fixing hedge cases for symboles to show up without needing to be guessed.  WOrking on bug that doesnt end game when word is guessed correctly that contains symbol

--- hangman-game/Program.cs
+++ hangman-game/Program.cs
@@ -108,6 +108,34 @@ namespace HangmanGame
                 {
                     Console.Write("  ");
                 }
+                else if (c.ToString() == ":")
+                {
+                    Console.Write(": ");
+                }
+                else if (c.ToString() == "-")
+                {
+                    Console.Write("- ");
+                }
+                else if (c.ToString() == "-")
+                {
+                    Console.Write("- ");
+                }
+                else if (c.ToString() == "?")
+                {
+                    Console.Write("? ");
+                }
+                else if (c.ToString() == ".")
+                {
+                    Console.Write(". ");
+                }
+                else if (c.ToString() == ",")
+                {
+                    Console.Write(", ");
+                }
+                else if (c.ToString() == "'")
+                {
+                    Console.Write("' ");
+                }
                 else
                 {
                     Console.Write("- ");
@@ -125,19 +153,277 @@ namespace HangmanGame
                 // Use random built in class to create another instance of it
                 Random random = new Random();
                 // create a list of words containing the following
-                List<string> wordDictionary = new List<string> { "CLEO IS A BABY", "CHLOE IS GRACE", "TWO EYE BADGIE", "KRISTEN IS A SUPERSTAR"};
+                List<string> wordDictionary = new List<string> { "The Shawshank Redemption",
+  "The Godfather",
+  "The Godfather: Part II",
+  "The Dark Knight",
+  "Schindler's List",
+  "Pulp Fiction",
+  "The Lord of the Rings: The Return of the King",
+  "The Good, the Bad and the Ugly",
+  "Fight Club",
+  "The Lord of the Rings: The Fellowship of the Ring",
+  "Forrest Gump",
+  "Star Wars: Episode V - The Empire Strikes Back",
+  "Inception",
+  "The Lord of the Rings: The Two Towers",
+  "One Flew Over the Cuckoo's Nest",
+  "Goodfellas",
+  "The Matrix",
+  "Seven Samurai",
+  "Star Wars: Episode IV - A New Hope",
+  "City of God",
+  "The Silence of the Lambs",
+  "It's a Wonderful Life",
+  "Life Is Beautiful",
+  "The Usual Suspects",
+  "Léon: The Professional",
+  "Saving Private Ryan",
+  "Spirited Away",
+  "American History X",
+  "Once Upon a Time in the West",
+  "Interstellar",
+  "Psycho",
+  "The Green Mile",
+  "Casablanca",
+  "City Lights",
+  "The Intouchables",
+  "Modern Times",
+  "Raiders of the Lost Ark",
+  "The Pianist",
+  "The Departed",
+  "Rear Window",
+  "Back to the Future",
+  "Whiplash",
+  "Gladiator",
+  "The Prestige",
+  "The Lion King",
+  "Memento",
+  "Apocalypse Now",
+  "Alien",
+  "The Great Dictator",
+  "Sunset Boulevard",
+  "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb",
+  "Cinema Paradiso",
+  "The Lives of Others",
+  "Grave of the Fireflies",
+  "Paths of Glory",
+  "Django Unchained",
+  "The Shining",
+  "WALL-E",
+  "American Beauty",
+  "The Dark Knight Rises",
+  "Princess Mononoke",
+  "Oldboy",
+  "Aliens",
+  "Witness for the Prosecution",
+  "Once Upon a Time in America",
+  "Das Boot",
+  "Citizen Kane",
+  "Dangal",
+  "Vertigo",
+  "North by Northwest",
+  "Star Wars: Episode VI - Return of the Jedi",
+  "Braveheart",
+  "Reservoir Dogs",
+  "Requiem for a Dream",
+  "Amélie",
+  "Like Stars on Earth",
+  "A Clockwork Orange",
+  "Your Name",
+  "Lawrence of Arabia",
+  "Dunkirk",
+  "Double Indemnity",
+  "Amadeus",
+  "Taxi Driver",
+  "Eternal Sunshine of the Spotless Mind",
+  "To Kill a Mockingbird",
+  "Full Metal Jacket",
+  "Singin' in the Rain",
+  "The Sting",
+  "Toy Story",
+  "Bicycle Thieves",
+  "Inglourious Basterds",
+  "The Kid",
+  "Snatch",
+  "Monty Python and the Holy Grail",
+  "Good Will Hunting",
+  "For a Few Dollars More",
+  "The Hunt",
+  "L.A. Confidential",
+  "Scarface",
+  "The Apartment",
+  "Metropolis",
+  "Rashomon",
+  "A Separation",
+  "Indiana Jones and the Last Crusade",
+  "My Father and My Son",
+  "Yojimbo",
+  "All About Eve",
+  "Batman Begins",
+  "Some Like It Hot",
+  "The Treasure of the Sierra Madre",
+  "Unforgiven",
+  "Downfall",
+  "Die Hard",
+  "Raging Bull",
+  "Children of Heaven",
+  "The Third Man",
+  "The Great Escape",
+  "Chinatown",
+  "Ikiru",
+  "Pan's Labyrinth",
+  "My Neighbor Totoro",
+  "The Gold Rush",
+  "Inside Out",
+  "Incendies",
+  "The Secret in Their Eyes",
+  "On the Waterfront",
+  "Judgment at Nuremberg",
+  "The Bridge on the River Kwai",
+  "Howl's Moving Castle",
+  "Blade Runner",
+  "The Seventh Seal",
+  "Lock, Stock and Two Smoking Barrels",
+  "Mr. Smith Goes to Washington",
+  "Casino",
+  "A Beautiful Mind",
+  "The Elephant Man",
+  "Wild Strawberries",
+  "V for Vendetta",
+  "The Wolf of Wall Street",
+  "The General",
+  "Warrior",
+  "Trainspotting",
+  "Dial M for Murder",
+  "Andrei Rublev",
+  "Gran Torino",
+  "Sunrise",
+  "Gone with the Wind",
+  "The Deer Hunter",
+  "The Bandit",
+  "Fargo",
+  "The Sixth Sense",
+  "La La Land",
+  "The Thing",
+  "The Big Lebowski",
+  "No Country for Old Men",
+  "Finding Nemo",
+  "Tokyo Story",
+  "Hacksaw Ridge",
+  "Cool Hand Luke",
+  "There Will Be Blood",
+  "Rebecca",
+  "Come and See",
+  "Rang De Basanti",
+  "The Passion of Joan of Arc",
+  "Kill Bill: Vol. 1",
+  "How to Train Your Dragon",
+  "Logan",
+  "Mary and Max",
+  "Gone Girl",
+  "Into the Wild",
+  "Shutter Island",
+  "A Wednesday",
+  "It Happened One Night",
+  "Life of Brian",
+  "Wild Tales",
+  "Platoon",
+  "The Wages of Fear",
+  "Hotel Rwanda",
+  "Network",
+  "Rush",
+  "In the Name of the Father",
+  "Stand by Me",
+  "Persona",
+  "Ben-Hur",
+  "The Grand Budapest Hotel",
+  "Mad Max: Fury Road",
+  "Memories of Murder",
+  "Spotlight",
+  "Million Dollar Baby",
+  "Jurassic Park",
+  "Butch Cassidy and the Sundance Kid",
+  "Stalker",
+  "Amores Perros",
+  "The Truman Show",
+  "The Maltese Falcon",
+  "Hachi: A Dog's Tale",
+  "Nausicaä of the Valley of the Wind",
+  "Paper Moon",
+  "The Princess Bride",
+  "The Nights of Cabiria",
+  "Before Sunrise",
+  "Prisoners",
+  "Harry Potter and the Deathly Hallows: Part 2",
+  "The Grapes of Wrath",
+  "Rocky",
+  "Catch Me If You Can",
+  "Diabolique",
+  "Touch of Evil",
+  "Gandhi",
+  "Star Wars: The Force Awakens",
+  "Donnie Darko",
+  "Monsters, Inc.",
+  "Annie Hall",
+  "The Terminator",
+  "Barry Lyndon",
+  "The Bourne Ultimatum",
+  "Sholay",
+  "The Wizard of Oz",
+  "Groundhog Day",
+  "Twelve Monkeys",
+  "The Best Years of Our Lives",
+  "Infernal Affairs",
+  "Paris, Texas",
+  "The Help",
+  "In the Mood for Love",
+  "Beauty and the Beast",
+  "The Battle of Algiers",
+  "Dog Day Afternoon",
+  "Pirates of the Caribbean: The Curse of the Black Pearl",
+  "Gangs of Wasseypur",
+  "The Handmaiden",
+  "What Ever Happened to Baby Jane?"};
 
                 // generate random number between 0 and number of words in wordDictionary, then assigns that word to randomWord
                 int index = random.Next(wordDictionary.Count);
-                String randomWord = wordDictionary[index];
+                String randomWord = wordDictionary[index].ToUpper();
 
                 // Print dashed lines that make up the word
                 foreach (char x in randomWord)
                 {
-                    //  Checks for spaces to display
-                    if(x.ToString() == " ")
+                    //  Checks for spaces, periods, dashes, and other symbols to display
+                    if (x.ToString() == " ")
                     {
                         Console.Write("  ");
+                    }
+                    else if (x.ToString() == ":")
+                    {
+                        Console.Write(": ");
+                    }
+                    else if (x.ToString() == "-")
+                    {
+                        Console.Write("- ");
+                    }else if (x.ToString() == "-")
+                    {
+                        Console.Write("- ");
+                    }
+                    else if (x.ToString() == "?")
+                    {
+                        Console.Write("? ");
+                    }
+                    else if (x.ToString() == ".")
+                    {
+                        Console.Write(". ");
+                    }
+                    else if (x.ToString() == ",")
+                    {
+                        Console.Write(", ");
+                    }
+                    else if (x.ToString() == "'")
+                    {
+                        Console.Write("' ");
                     }
                     else
                     {
@@ -174,9 +460,6 @@ namespace HangmanGame
                         if (currentLettersGuessed.Contains(letterGuessed))
                         {
                             Console.Write("\r\n You have already guessed this letter");
-                            //printHangman(amountOfTimesWrong);
-                            //currentLettersRight = printWord(currentLettersGuessed, randomWord);
-                            //printLines(randomWord);
                         }
                         else
                         {
@@ -196,7 +479,7 @@ namespace HangmanGame
                                 Console.Write("\r\n");
                                 printLines(randomWord);
                             }
-                            // User was wrong af
+                            // User was wrong
                             else
                             {
                                 amountOfTimesWrong += 1;
@@ -212,22 +495,46 @@ namespace HangmanGame
                     }
                     
                 }
-                // End game, ask to play again
-                Console.WriteLine("\r\nGame is over! Play again?... [Y]/[N]");
-                string playAgain = Console.ReadLine().ToUpper();
+                if(amountOfTimesWrong == 6)
+                {
+                    // End game if word not guessed, displays and asks to play again
+                    Console.WriteLine("\r\nGame is over! The word was " + randomWord);
+                    Console.WriteLine("\r\nPlay again ? ... [Y] /[N]");
+                    string playAgain = Console.ReadLine().ToUpper();
+                    //gameOver(playAgain);  <- turn ito owm method to simplify
+                    if (playAgain == "Y")
+                    {
+                        continue;
+                    }
+                    else if (playAgain == "N")
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                } else
+                {
+                    // End game if word is guesed, ask to play again
+                    Console.WriteLine("\r\nCongrats, you got it!");
+                    Console.WriteLine("\r\nPlay again ? ... [Y] /[N]");
+                    string playAgain = Console.ReadLine().ToUpper();
+                    //gameOver(playAgain);  <- turn ito owm method to simplify
+                    if (playAgain == "Y")
+                    {
+                        continue;
+                    }
+                    else if (playAgain == "N")
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
 
-                if (playAgain == "Y")
-                {
-                    continue;
-                }
-                else if (playAgain == "N")
-                {
-                    return;
-                }
-                else
-                {
-                    return;
-                }
             }
 
         }
